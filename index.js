6c33b7a28b13c81fd36c543369552de91326c1ba
Emilio <emilioplatzer@gmail.com>
2015-06-15 17:13:19 -0300

    Primera version

---
+++ index.js
@@ -0,0 +1,9 @@
+/*!
+ * qaControl
+ * 2015 Codenautas
+ * GNU Licensed
+ */
+
+exports = module.exports = function qaControl(opts){
+    throw new Error("Not implemented yet!");
+}
---
+++ test/test.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var expect = require('expect.js');
+var qac = require('..');
+
+describe('qa-control', function(){
+    describe('basic test', function(){
+        it('first test', function(){
+            throw new Error('no tests');
+        });
+    });
+});
