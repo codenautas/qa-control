f8c1752716f8d61d010ca52c46ebd1d6c4ae1094
Diego F.(i7) <diegoefe@gmail.com>
2015-12-29 12:35:42 -0300

    Subo coverage (bin/0.0.2/definition.js estaba bajo)

--- test/test.js
+++ test/test.js
@@ -418,12 +418,20 @@ var fixtures=[{
     title:'must reject files without correct "use strict" (#43)',
     test:'wrong_use_strict_spelling_in_file_1',
     change:function(info){
-        info.files['simple.js'].content =
-            info.files['simple.js'].content.replace("\tuse strict", "use spirit");
+        info.files['simple.js'].content = info.files['simple.js'].content.replace("\tuse strict", "use spirit");
     },
     expected:[
         { warning:'wrong_use_strict_spelling_in_file_1',params:['simple.js'] }
     ]
+},{
+    base:'stable-project',
+    title:'coverage for version "0.0.2"',
+    scoring:true,
+    change:function(info){
+        info.files['stable-project.js'].content = info.files['stable-project.js'].content.replace("/*eslint-disable no-console */\n", "");
+        info.packageJson['qa-control']['package-version']='0.0.2';
+    },
+    expected: []
 }];
 
 //
