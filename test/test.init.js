fd4798a444554b6e99216a30ffc738642276ff1b
Diego F.(i7) <diegoefe@gmail.com>
2016-02-17 09:54:08 -0300

    init: Creando estructura para leer parámetros

--- test/test.init.js
+++ test/test.init.js
@@ -10,14 +10,20 @@ var Path = require('path');
 var qci = require('../bin/qac-init.js')
 
 describe('qa-control --init', function(){
-    it('dummy', function(done){
-        // return qci.promptForVar('var1', 'def1').then(function(val) {
-            // console.log("var1", val);
-            // done();
-        // }).catch(function(err) {
-            // console.log("error", err);
-        // });
-        expect(true).to.be.ok();
+    it('Param', function(done){
+        var p1 = new qci.Param('v1', 'def1', function(curResult) {});
+        var p2 = new qci.Param('v2', 'def2', function(curResult) {
+            if(curResult.v1) {
+                this.def = 'using '+curResult.v1;
+            }
+        });
+        expect(p1.name).to.eql('v1');
+        expect(p1.def).to.eql('def1');
+        expect(p2.name).to.eql('v2');
+        expect(p2.def).to.eql('def2');
+        var curResult = { v1: 'val1' };
+        p2.init(curResult);
+        expect(p2.def).to.eql('using val1');
         done();
     }, function(err) {
         done(err);
